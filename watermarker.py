e6ecdade23944e5823a258ddeb6fcad3b8d7d965
Imafe <v.sanderamsterdam@gmail.com>
2022-03-03 00:05:43 +0100

    add grid with adjustable angle, lines, offsets, color

--- watermarker.py
+++ watermarker.py
@@ -33,6 +33,11 @@ FONT_RGB = FONT_RGB if FONT_RGB else tuple(np.random.choice(range(256), size=3))
 FONT_SIZE = 50
 WATERMARK_TEXT = "Getty Images"
 
+def getRandomColor():
+    return tuple(np.random.choice(range(256), size=3)) + (random.choice(range(70, 90)),)
+
+def radians(deg):
+    return deg * math.pi / 180
 
 class Watermarker:
     def __init__(self, file_path, font=ImageFont.truetype("resources/calibri.ttf", FONT_SIZE)):
@@ -47,6 +52,23 @@ class Watermarker:
 
         self.draw = ImageDraw.Draw(self.image, "RGBA")
 
+    def rotated_text(self, angle, xy, text, fill, upscale=False, center_coords=False, *args, **kwargs):
+        # Upscale increases text quality when rotated at none 90 degree angle
+        max_dim = max(*self.image.size)
+        mask = Image.new('L', (max_dim*2, max_dim*2), 0)
+        draw = ImageDraw.Draw(mask)
+        if center_coords: 
+            half_text_height = int(draw.textsize(text, self.font)[1] / 2)
+            xy = (xy[0], xy[1] - (half_text_height + math.cos(radians(angle)) * half_text_height))
+
+        draw.text((max_dim, max_dim), text, 255, *args, **kwargs)
+        rotated_mask = mask.rotate(angle, resample=(3 if upscale else 0))   
+
+        mask_xy = (max_dim - xy[0], max_dim - xy[1])
+        final_mask = rotated_mask.crop(mask_xy + (mask_xy[0] + self.image.width, mask_xy[1] + self.image.height))
+
+        self.image.paste(Image.new('RGBA', self.image.size, fill), final_mask)
+
     def add_text(self):
         w, h = self.font.getsize(WATERMARK_TEXT)
         padding = 20
@@ -60,6 +82,30 @@ class Watermarker:
         self.draw.rectangle((x1 - padding, y1 - padding, x2 + padding, y2 + padding), BG_RGBA, BG_RGBA, 0)
 
         self.draw.text((x1, y1), WATERMARK_TEXT, fill=FONT_RGB, font=self.font)
+        # self.rotated_text(0, (x1, y1), WATERMARK_TEXT, fill=FONT_RGB, font=self.font, upscale=False)
+
+    def draw_angled_line_y(self, xy, y2, angle, fill=BG_RGBA):
+        xy2 = (xy[0] + int((y2 - xy[1]) / math.tan(radians(angle))), y2) if angle else (xy[0], y2)
+        self.draw.line((*xy, xy2), fill, width=4)
+
+    def draw_angled_line_x(self, xy, x2, angle, fill=BG_RGBA):
+        xy2 = (x2, xy[1] + int((x2 - xy[0]) * math.tan(radians(angle))))
+        self.draw.line((*xy, xy2), fill, width=4)
+
+    def draw_grid(self, angle, lines=5, offset_x=0, offset_y=0, line_color=BG_RGBA):
+        added_angle = 45 if (angle := angle % 45) else 0 # Cursed python code
+        start_X, start_Y = -self.image.width * 2 + offset_x, -self.image.height * 2 + offset_y
+        for i in range(total_lines := lines * 3):
+            spacing_W, spacing_H = self.image.width * 3 / total_lines, self.image.height * 3 / total_lines
+            self.draw_angled_line_y((int(start_X + i * spacing_W), 0), self.image.height, angle + added_angle, fill=line_color)
+            self.draw_angled_line_x((self.image.width, int(start_Y + i * spacing_H)), 0, -angle, fill=line_color)
+
+    def draw_randomized_grid(self):
+        angle = random.randrange(0, 90)
+        lines = random.randrange(3,10)
+        offset_x, offset_y = random.randrange(0,100), random.randrange(0,100)
+        fill_color = getRandomColor()
+        self.draw_grid(angle, lines, offset_x, offset_y, fill_color)
 
     def add_simple_grid(self):
         spacing = self.image.width / 5
@@ -72,8 +118,9 @@ class Watermarker:
             x1 = x0 + self.image.height + offset_lower_x
             self.draw.line(((x0, 0), (x1, self.image.height)), BG_RGBA, width=2)
             self.draw.line(((x0, self.image.height), (x1, 0)), BG_RGBA, width=2)
+            # self.rotated_text(45, (int((x1 - x0) / 2) + x0, int(self.image.height / 2)), "TEST", fill=FONT_RGB, font=self.font, upscale=False, center_coords=True)
 
-    def save(self, output_dir):
+    def save(self, output_dir=OUTPUT_DIR):
         self.image.save(f"{output_dir}/{self.filename}")
         logger.debug(f"Added watermark to {self.filename}")
 
@@ -91,7 +138,6 @@ if __name__ == "__main__":
             exit(1)
 
     os.makedirs(OUTPUT_DIR)
-
     skipped = []
     for i in tqdm(range(len(jpgs))):
         SEED += 1
@@ -103,7 +149,7 @@ if __name__ == "__main__":
             skipped += jpgs[i]
             continue
         watermarker.add_text()
-        watermarker.add_simple_grid()
-        watermarker.save(output_dir=OUTPUT_DIR)
+        watermarker.draw_randomized_grid()
+        watermarker.save()
 
     logger.info(f"Processed {len(jpgs)} images. Skipped {len(skipped)}.")
