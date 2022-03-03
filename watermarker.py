cd272dea9420181c87afba42b0ef46cdf651b639
Imafe <v.sanderamsterdam@gmail.com>
2022-03-03 20:03:39 +0100

    Added randomized lines, text and grid

--- watermarker.py
+++ watermarker.py
@@ -3,7 +3,7 @@ import math
 import os.path
 import random
 import shutil
-
+from string import ascii_letters
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from prompt_toolkit.shortcuts import confirm
@@ -34,10 +34,13 @@ FONT_SIZE = 50
 WATERMARK_TEXT = "Getty Images"
 
 def getRandomColor():
-    return tuple(np.random.choice(range(256), size=3)) + (random.choice(range(70, 90)),)
+    return tuple(np.random.choice(range(256), size=3)) + (random.choice(range(40, 90)),)
 
 def radians(deg):
     return deg * math.pi / 180
+
+def coinflip():
+    return np.random.randint(2) % 2 == 0
 
 class Watermarker:
     def __init__(self, file_path, font=ImageFont.truetype("resources/calibri.ttf", FONT_SIZE)):
@@ -61,7 +64,7 @@ class Watermarker:
             half_text_height = int(draw.textsize(text, self.font)[1] / 2)
             xy = (xy[0], xy[1] - (half_text_height + math.cos(radians(angle)) * half_text_height))
 
-        draw.text((max_dim, max_dim), text, 255, *args, **kwargs)
+        draw.text((max_dim, max_dim), text, 255, font=self.font,  *args, **kwargs)
         rotated_mask = mask.rotate(angle, resample=(3 if upscale else 0))   
 
         mask_xy = (max_dim - xy[0], max_dim - xy[1])
@@ -84,28 +87,55 @@ class Watermarker:
         self.draw.text((x1, y1), WATERMARK_TEXT, fill=FONT_RGB, font=self.font)
         # self.rotated_text(0, (x1, y1), WATERMARK_TEXT, fill=FONT_RGB, font=self.font, upscale=False)
 
-    def draw_angled_line_y(self, xy, y2, angle, fill=BG_RGBA):
+    def draw_angled_line_y(self, xy, y2, angle, fill=BG_RGBA, width=4):
         xy2 = (xy[0] + int((y2 - xy[1]) / math.tan(radians(angle))), y2) if angle else (xy[0], y2)
-        self.draw.line((*xy, xy2), fill, width=4)
+        self.draw.line((*xy, xy2), fill, width=width)
 
-    def draw_angled_line_x(self, xy, x2, angle, fill=BG_RGBA):
+    def draw_angled_line_x(self, xy, x2, angle, fill=BG_RGBA, width=4):
         xy2 = (x2, xy[1] + int((x2 - xy[0]) * math.tan(radians(angle))))
-        self.draw.line((*xy, xy2), fill, width=4)
+        self.draw.line((*xy, xy2), fill, width=width)
 
-    def draw_grid(self, angle, lines=5, offset_x=0, offset_y=0, line_color=BG_RGBA):
+    def draw_grid(self, angle, lines=5, offset_x=0, offset_y=0, line_color=BG_RGBA, width=4):
         added_angle = 45 if (angle := angle % 45) else 0 # Cursed python code
         start_X, start_Y = -self.image.width * 2 + offset_x, -self.image.height * 2 + offset_y
         for i in range(total_lines := lines * 3):
             spacing_W, spacing_H = self.image.width * 3 / total_lines, self.image.height * 3 / total_lines
-            self.draw_angled_line_y((int(start_X + i * spacing_W), 0), self.image.height, angle + added_angle, fill=line_color)
-            self.draw_angled_line_x((self.image.width, int(start_Y + i * spacing_H)), 0, -angle, fill=line_color)
+            self.draw_angled_line_y((int(start_X + i * spacing_W), 0), self.image.height, angle + added_angle, fill=line_color, width=width)
+            self.draw_angled_line_x((self.image.width, int(start_Y + i * spacing_H)), 0, -angle, fill=line_color, width=width)
 
     def draw_randomized_grid(self):
         angle = random.randrange(0, 90)
         lines = random.randrange(3,10)
         offset_x, offset_y = random.randrange(0,100), random.randrange(0,100)
         fill_color = getRandomColor()
-        self.draw_grid(angle, lines, offset_x, offset_y, fill_color)
+        width = random.randrange(1,10)
+        self.draw_grid(angle, lines, offset_x, offset_y, fill_color, width)
+
+    def draw_random_lines(self):
+        for _ in range(random.randrange(30,40)):
+            angle = random.randrange(-45, 45)
+            width = random.randrange(3, 10)
+            fill_color = getRandomColor()
+            side = coinflip()
+            if coinflip(): 
+                self.draw_angled_line_y((random.randrange(0,self.image.width), 0 if side else self.image.height), self.image.height if side else 0, angle, fill_color, width)
+            else: 
+                self.draw_angled_line_x((0 if side else self.image.width, random.randrange(0,self.image.height)), self.image.width if side else 0, angle, fill_color, width)
+
+    def generate_gibberish(self, length):
+        return "".join([random.choice(ascii_letters + " ") for x in range(length)])
+
+    def add_random_text(self):
+        random_words = random.randrange(2,6)
+        for _ in range(random_words):
+            length = random.randrange(5,10)
+            word = self.generate_gibberish(length)
+            angle = random.randrange(0, 360)
+            fill = getRandomColor()
+            x = random.randrange(0, self.image.width)
+            y = random.randrange(0, self.image.height)
+            self.rotated_text(angle, (x,y), word, fill)
+
 
     def add_simple_grid(self):
         spacing = self.image.width / 5
@@ -150,6 +180,8 @@ if __name__ == "__main__":
             continue
         watermarker.add_text()
         watermarker.draw_randomized_grid()
+        watermarker.draw_random_lines()
+        watermarker.add_random_text()
         watermarker.save()
 
     logger.info(f"Processed {len(jpgs)} images. Skipped {len(skipped)}.")
