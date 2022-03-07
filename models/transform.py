88c716475c557abd0b6e45b7ae7f69eb89d073c4
Jop Zitman <jop-zitman@hotmail.com>
2022-03-07 18:41:59 +0100

    Add a torchvision Dataset class to load our dataset (and apply watermarkers/chunks)

---
+++ models/transform.py
@@ -0,0 +1,10 @@
+import torch
+import torchvision.transforms as T
+
+TRANSFORM = T.Compose(
+    [
+        T.PILToTensor(),
+        T.RandomCrop(128, pad_if_needed=False),
+        T.Lambda(lambda tensor: torch.divide(tensor, 255)),
+    ]
+)
