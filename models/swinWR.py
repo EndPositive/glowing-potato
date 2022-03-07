05df76613c2c4bc7ff37bdea07f4f2573082ce87
Tiberiu Iancu <tiberiuiancu@Tiberius-MacBook-Air.local>
2022-03-07 15:20:53 +0100

    Reformat to abstract class structure for compatibility between tf and torch models

--- models/swinWR.py
+++ models/swinWR.py
@@ -1,19 +1,21 @@
+import sys
 from pathlib import Path
+sys.path.append(Path(__file__).parents[1].absolute().as_posix())
 
 import numpy as np
 import torch
 from PIL import Image
 
-import preprocessing.formatter as processing
 from swinir.models.network_swinir import SwinIR
+from wrmodel import WRmodel
 
-
-class SwinWR:
-    def __init__(self, train_last_layer_only=False):
+class SwinWR(WRmodel):
+    def __init__(self, image_size=(128, 128), train_last_layer_only=False):
+        super().__init__(image_size)
         self.model = SwinIR(
             upscale=1,
             in_chans=3,
-            img_size=128,
+            img_size=self.image_size[0],
             window_size=8,
             img_range=1.0,
             depths=[6, 6, 6, 6, 6, 6],
@@ -37,32 +39,25 @@ class SwinWR:
             self.model.conv_last.weight.requires_grad = True
             self.model.conv_last.bias.requires_grad = True
 
-    @staticmethod
-    def _output_to_array(x):
+    def _decode_output(self, x):
         x = x.cpu().detach().numpy()
         x = np.transpose(x, (0, 2, 3, 1))
         return (x * 255).astype(np.uint8)
 
-    @staticmethod
-    def _array_to_input(x):
+    def _encode_input(self, x):
         if len(x.shape) == 3:
             x = np.expand_dims(x, 0)
         x = np.transpose(x, (0, 3, 1, 2)) / 255  # convert to channels first
-        return torch.from_numpy(x).float().to("cpu")  # add batch dimension
+        return torch.from_numpy(x).float().to("cpu")
 
     def __call__(self, img):
-        if isinstance(img, str) or isinstance(img, Path):
-            img = Image.open(img)
+        return self.predict(img)
 
-        padded, padding = processing.pad(img, (128, 128), return_padding=True)
-        split = processing.split_image(padded, (128, 128))
-        output = self._output_to_array(self.model(self._array_to_input(split)))
-        unsplit = processing.unsplit_image(output, padded.shape[0])
-        unpadded = processing.unpad(unsplit, padding)
-        return unpadded
+    def forward_pass(self, x):
+        return self.model(x)
 
 
 if __name__ == "__main__":
     with torch.no_grad():
         model = SwinWR()
-        Image.fromarray(model("resources/out/input/0c3ee986fa326b1a_7.jpg")).show()
+        Image.fromarray(model("../resources/dataset/input/0c3ee986fa326b1a_7.jpg")).show()
---
+++ models/wrmodel.py
@@ -0,0 +1,59 @@
+import sys; sys.path.append('..')
+
+from pathlib import Path
+from PIL import Image
+from abc import abstractclassmethod
+from collections.abc import Iterable
+from preprocessing import formatter as processing
+import numpy as np
+
+
+class WRmodel:
+    def __init__(self, image_size=(128, 128)):
+        self.image_size = image_size
+
+    @abstractclassmethod
+    def train_epoch(self):
+        pass
+
+    @abstractclassmethod
+    def forward_pass(self):
+        pass
+
+    @abstractclassmethod
+    def _encode_input(self, x):
+        pass
+
+    @abstractclassmethod
+    def _decode_output(self, y):
+        pass
+
+    def predict(self, img):
+        if isinstance(img, str) or isinstance(img, Path):
+            img = Image.open(img)
+            img = np.asarray(img)
+
+        # shape should be image or batch of images
+        assert(len(np.shape(img)) == 3 or len(np.shape(img)) == 4)
+
+        # predict multiple images
+        if len(np.shape(img)) == 4:
+            return [self.predict(x) for x in img]
+
+        # pad to align to img size
+        padded, padding = processing.pad(img, self.image_size, return_padding=True)
+
+        # split image in multiple blocks, if too big
+        split = processing.split_image(padded, self.image_size)
+
+        # normalize and get input in tensor form
+        encoded_input = self._encode_input(split)
+
+        # run the network and decode the output (get numpy from tensor and un-normalize)
+        output = self._decode_output(self.forward_pass(encoded_input))
+
+        # piece image back together
+        unsplit = processing.unsplit_image(output, padded.shape[0])
+
+        # unpad and return
+        return processing.unpad(unsplit, padding)
