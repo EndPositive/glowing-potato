aae0686bbd997a5ef68e4b4ad3f5843e3c96ee35
Tibi <tiberiu.iancu@gmail.com>
2022-03-11 16:42:01 +0100

    Reformatting

--- models/swin_wr.py
+++ models/swin_wr.py
@@ -1,6 +1,3 @@
-import os
-
-import numpy as np
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
@@ -70,137 +67,8 @@ class SwinWR(SwinWRBase):
         self._lossfn = nn.L1Loss()
 
         # define device to run on
-        self._device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self._model.to(self._device)
         print(f"Running model on {self._device}")
-
-    def _decode_output(self, x):
-        x = x.cpu().detach().numpy()
-        x = np.transpose(x, (0, 2, 3, 1))
-        return (x * 255).astype(np.uint8)
-
-    def _encode_input(self, x):
-        if len(x.shape) == 3:
-            x = np.expand_dims(x, 0)
-        x = np.transpose(x, (0, 3, 1, 2)) / 255  # convert to channels first
-        return torch.from_numpy(x).float().to("cpu")
-
-    def forward(self, x):
-        return self._model(x)
-
-    def save(self, path):
-        torch.save(self._model.state_dict(), path)
-
-    def load(self, path):
-        self._model.load_state_dict(torch.load(path))
-
-    def train_epoch(self, dataloader: DataLoader, epoch=0, log_every=-1):
-        epoch_loss = 0
-        running_loss = 0
-        for i, (x, y_hat) in enumerate(iter(dataloader)):
-            # zero the parameter gradients
-            self._optimizer.zero_grad()
-
-            # forward + backward + optimize
-            y = self(x)
-            loss = self._lossfn(y, y_hat)
-            loss.backward()
-            self._optimizer.step()
-
-            # add loss to return
-            epoch_loss += loss.item()
-
-            # print statistics
-            if log_every > 0:
-                running_loss += loss.item()
-                if (i + 1) % log_every == 0:
-                    print(
-                        f"[{epoch + 1}, {i + 1:5d}] "
-                        "loss: {running_loss / log_every:.3f}"
-                    )
-                    running_loss = 0
-
-        return epoch_loss / len(dataloader)
-
-    def test(self, testset: DataLoader):
-        with torch.no_grad():
-            return np.mean(
-                [
-                    self._lossfn(self.forward_pass(x), y_hat).item()
-                    for x, y_hat in iter(testset)
-                ]
-            )
-
-    def train(
-        self,
-        n_epochs=-1,
-        val_stop=5,
-        save_path=".",
-        save_every=-1,
-        data_shuffle=True,
-        data_num_workers=0,
-        batch_size=16,
-    ):
-        # load train and validation sets
-        train_data_loader = DataLoader(
-            ChunkedWatermarkedSet(
-                data_set_type=DataSetType.Training, device=self._device
-            ),
-            batch_size=batch_size,
-            shuffle=data_shuffle,
-            num_workers=data_num_workers,
-        )
-
-        validation_data_loader = DataLoader(
-            ChunkedWatermarkedSet(
-                data_set_type=DataSetType.Validation, device=self._device
-            ),
-            batch_size=batch_size,
-            shuffle=data_shuffle,
-            num_workers=data_num_workers,
-        )
-
-        # make sure save path exists
-        os.makedirs(save_path, exist_ok=True)
-
-        epoch = 0
-        train_losses = []
-        val_losses = []
-        while n_epochs < 0 or epoch < n_epochs:
-            # train the model one epoch
-            train_loss = self.train_epoch(train_data_loader, epoch)
-
-            # test on the validation set
-            val_loss = self.test(validation_data_loader)
-
-            # print epoch summary
-            print(f"Epoch {epoch} summary:")
-            print(f"Train loss: {train_loss}")
-            print(f"Validation loss: {val_loss}\n")
-
-            # log losses
-            val_losses.append(val_loss)
-            train_losses.append(train_loss)
-
-            # save model if necessary
-            if save_every > 0 and (epoch + 1) % save_every == 0:
-                self.save(os.path.join(save_path, f"ckpt_{epoch}.pth"))
-
-            # if validation loss hasn't improved in val_loss epochs, stop training
-            if (
-                0 < val_stop <= len(val_losses)
-                and np.mean(val_losses[-val_stop + 1 :]) > val_losses[-val_stop]
-            ):
-                print(
-                    f"Validation loss hasn't improved in {val_stop} epochs. "
-                    "Stopping training..."
-                )
-                break
-
-            epoch += 1
-
-        self.save(os.path.join(save_path, "final.pth"))
-        return train_losses, val_losses
 
     def precompute_dataset(self, batch_size=64, output_to=preprocessing.OUTPUT_DIR_PRECOMPUTED):
         train_data_loader = DataLoader(
@@ -231,4 +99,4 @@ class SwinWR(SwinWRBase):
 
 if __name__ == "__main__":
     m = SwinWR()
-    m.precompute_dataset()
+    m.train()
--- models/swin_wr_base.py
+++ models/swin_wr_base.py
@@ -1,34 +1,136 @@
-from abc import abstractmethod
 from pathlib import Path
+import os
 
 import numpy as np
+from torch.optim import Optimizer
+import torch
 from PIL import Image
 from torch.utils.data import DataLoader
 from torch import nn
+from datasets.chunked_watermarked_set import ChunkedWatermarkedSet, DataSetType
 
 from preprocessing import formatter as processing
 
 
 class SwinWRBase(nn.Module):
+    _lossfn: nn.Module
+    _optimizer: Optimizer
+    _model: nn.Module
+
     def __init__(self, image_size=(128, 128)):
         super().__init__()
+        self._device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.image_size = image_size
 
-    @abstractmethod
-    def train_epoch(self, dataloader: DataLoader, epoch=0, verbose=True):
-        pass
+    def train_epoch(self, dataloader: DataLoader, epoch=0, log_every=-1):
+        epoch_loss = 0
+        running_loss = 0
+        for i, (x, y_hat) in enumerate(iter(dataloader)):
+            # zero the parameter gradients
+            self._optimizer.zero_grad()
 
-    @abstractmethod
-    def forward(self, x):
-        pass
+            # forward + backward + optimize
+            y = self(x)
+            loss = self._lossfn(y, y_hat)
+            loss.backward()
+            self._optimizer.step()
 
-    @abstractmethod
+            # add loss to return
+            epoch_loss += loss.item()
+
+            # print statistics
+            if log_every > 0:
+                running_loss += loss.item()
+                if (i + 1) % log_every == 0:
+                    print(
+                        f"[{epoch + 1}, {i + 1:5d}] "
+                        "loss: {running_loss / log_every:.3f}"
+                    )
+                    running_loss = 0
+
+        return epoch_loss / len(dataloader)
+
+    def train(
+        self,
+        n_epochs=-1,
+        val_stop=5,
+        save_path=".",
+        save_every=-1,
+        data_shuffle=True,
+        data_num_workers=0,
+        batch_size=16,
+    ):
+        # load train and validation sets
+        train_data_loader = DataLoader(
+            ChunkedWatermarkedSet(
+                data_set_type=DataSetType.Training, device=self._device
+            ),
+            batch_size=batch_size,
+            shuffle=data_shuffle,
+            num_workers=data_num_workers,
+        )
+
+        validation_data_loader = DataLoader(
+            ChunkedWatermarkedSet(
+                data_set_type=DataSetType.Validation, device=self._device
+            ),
+            batch_size=batch_size,
+            shuffle=data_shuffle,
+            num_workers=data_num_workers,
+        )
+
+        # make sure save path exists
+        os.makedirs(save_path, exist_ok=True)
+
+        epoch = 0
+        train_losses = []
+        val_losses = []
+        while n_epochs < 0 or epoch < n_epochs:
+            # train the model one epoch
+            train_loss = self.train_epoch(train_data_loader, epoch)
+
+            # test on the validation set
+            val_loss = self.test(validation_data_loader)
+
+            # print epoch summary
+            print(f"Epoch {epoch} summary:")
+            print(f"Train loss: {train_loss}")
+            print(f"Validation loss: {val_loss}\n")
+
+            # log losses
+            val_losses.append(val_loss)
+            train_losses.append(train_loss)
+
+            # save model if necessary
+            if save_every > 0 and (epoch + 1) % save_every == 0:
+                self.save(os.path.join(save_path, f"ckpt_{epoch}.pth"))
+
+            # if validation loss hasn't improved in val_loss epochs, stop training
+            if (
+                0 < val_stop <= len(val_losses)
+                and np.mean(val_losses[-val_stop + 1:]) > val_losses[-val_stop]
+            ):
+                print(
+                    f"Validation loss hasn't improved in {val_stop} epochs. "
+                    "Stopping training..."
+                )
+                break
+
+            epoch += 1
+
+        self.save(os.path.join(save_path, "final.pth"))
+        return train_losses, val_losses
+
+    def _decode_output(self, x):
+        x = x.cpu().detach().numpy()
+        x = np.transpose(x, (0, 2, 3, 1))
+        return (x * 255).astype(np.uint8)
+
     def _encode_input(self, x):
-        pass
-
-    @abstractmethod
-    def _decode_output(self, y):
-        pass
+        if len(x.shape) == 3:
+            x = np.expand_dims(x, 0)
+        x = np.transpose(x, (0, 3, 1, 2)) / 255  # convert to channels first
+        return torch.from_numpy(x).float().to("cpu")
 
     def predict(self, img):
         if isinstance(img, str) or isinstance(img, Path):
@@ -60,14 +162,20 @@ class SwinWRBase(nn.Module):
         # unpad and return
         return processing.unpad(unsplit, padding)
 
-    @abstractmethod
     def save(self, path):
-        pass
+        torch.save(self._model.state_dict(), path)
 
-    @abstractmethod
     def load(self, path):
-        pass
+        self._model.load_state_dict(torch.load(path))
 
-    @abstractmethod
-    def train(self, n_epochs=0):
-        pass
+    def test(self, testset: DataLoader):
+        with torch.no_grad():
+            return np.mean(
+                [
+                    self._lossfn(self(x), y_hat).item()
+                    for x, y_hat in iter(testset)
+                ]
+            )
+
+    def forward(self, x):
+        return self._model(x)
